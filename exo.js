d25d3ea93a7d74dd52b9042dc905e21f504473ac
Christian Østergaard Laursen <cptmack@cptmack.com>
2013-06-30 10:40:55 +0200

    Implemented cluster functionality
    The webservice will now spawn a childprocess for each CPU core
    available - which will improve load-handling in a linear fashion.

--- exo.js
+++ exo.js
@@ -10,8 +10,8 @@ var date = new Date();
 var path = require('path')
 var app = module.exports = express();
 //Cluster
-/*var cluster = require('cluster');
-var numCPUs = require('os').cpus().length; */
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 //Configuration
 var uploadDirectory = './uploads/';
@@ -28,26 +28,38 @@ function makeid()	{
 }
 
 
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+  console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
 //BodyParser
-app.use(express.bodyParser({keepExtensions: true, uploadDir:uploadDirectory}))
+	app.use(express.bodyParser({keepExtensions: true, uploadDir:uploadDirectory}))
+	
+	// Form
+	app.get('/', function(req, res)	{
+	  res.send('<form method="post" enctype="multipart/form-data">'
+	    + '<p>Image: <input type="file" name="image" /></p>'
+	    + '<p><input type="submit" value="Upload" /></p>'
+	    + '</form>');
+	});
+	
+	// Generate new ID (filename) for uploaded file)
+	app.post('/', function(req, res, next)	{
+		var newFileName = makeid() + path.extname(req.files.image.name)
+	  	fs.renameSync(req.files.image.path, uploadDirectory + newFileName);
+	  	res.json(200, {imageID: newFileName})
+	});
+	
+	if (!module.parent)	{
+	  	app.listen(3000);
+	  	console.log('Node started on ' + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " - " + date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear());
+	}
 
-// Form
-app.get('/', function(req, res)	{
-  res.send('<form method="post" enctype="multipart/form-data">'
-    + '<p>Image: <input type="file" name="image" /></p>'
-    + '<p><input type="submit" value="Upload" /></p>'
-    + '</form>');
-});
-
-// Generate new ID (filename) for uploaded file)
-app.post('/', function(req, res, next)	{
-	var newFileName = makeid() + path.extname(req.files.image.name)
-  	fs.renameSync(req.files.image.path, uploadDirectory + newFileName);
-  	res.json(200, {imageID: newFileName})
-});
-
-if (!module.parent)	{
-  	app.listen(3000);
-  	console.log('Node started on ' + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " - " + date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear());
 }
+
 
